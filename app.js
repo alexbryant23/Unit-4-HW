a2870fb73c60d169363e10e717a46380ebf132d3
Alex Bryant <alexbryant@Alexs-MacBook-Pro-2.local>
2018-09-12 13:15:15 -0400

    final product

---
+++ app.js
@@ -0,0 +1,232 @@
+const employeeList = [{
+    name: 'Jan',
+    officeNum: 1,
+    phoneNum: '222-222-2222'
+  },
+  {
+    name: 'Juan',
+    officeNum: 304,
+    phoneNum: '489-789-8789'
+  },
+  {
+    name: 'Margie',
+    officeNum: 789,
+    phoneNum: '789-789-7897'
+  },
+  {
+    name: 'Sara',
+    officeNum: 32,
+    phoneNum: '222-789-4654'
+  },
+  {
+    name: 'Tyrell',
+    officeNum: 3,
+    phoneNum: '566-621-0452'
+  },
+  {
+    name: 'Tasha',
+    officeNum: 213,
+    phoneNum: '789-766-5675'
+  },
+
+  {
+    name: 'Ty',
+    officeNum: 211,
+    phoneNum: '789-766-7865'
+  },
+  {
+    name: 'Sarah',
+    officeNum: 345,
+    phoneNum: '222-789-5231'
+  }
+];
+const printThis = (e) => {
+  e.preventDefault();
+  $('#content').empty();
+  $('#verifyme').removeClass('show');
+  $('#lookupme').removeClass('show');
+  $('#containsme').removeClass('show');
+  $('#deleteme').removeClass('show');
+  $('#addme').removeClass('show');
+  $('#updateme').removeClass('show');
+  $('#printme').addClass('show');
+};
+const verifyThis = (e) => {
+  e.preventDefault();
+  $('#content').empty();
+  $('#printme').removeClass('show');
+  $('#lookupme').removeClass('show');
+  $('#containsme').removeClass('show');
+  $('#addme').removeClass('show');
+  $('#updateme').removeClass('show');
+  $('#deleteme').removeClass('show');
+  $('#verifyme').addClass('show');
+};
+const lookupThis = (e) => {
+  e.preventDefault();
+  $('#content').empty();
+  $('#printme').removeClass('show');
+  $('#verifyme').removeClass('show');
+  $('#addme').removeClass('show');
+  $('#containsme').removeClass('show');
+  $('#updateme').removeClass('show');
+  $('#deleteme').removeClass('show');
+  $('#lookupme').addClass('show');
+};
+const addThis = (e) => {
+  e.preventDefault();
+  $('#content').empty();
+  $('#verifyme').removeClass('show');
+  $('#printme').removeClass('show');
+  $('#lookupme').removeClass('show');
+  $('#containsme').removeClass('show');
+  $('#updateme').removeClass('show');
+  $('#deleteme').removeClass('show');
+  $('#addme').addClass('show');
+};
+const updateThis = (e) => {
+  e.preventDefault();
+  $('#content').empty();
+  $('#verifyme').removeClass('show');
+  $('#lookupme').removeClass('show');
+  $('#addme').removeClass('show');
+  $('#containsme').removeClass('show');
+  $('#printme').removeClass('show');
+  $('#deleteme').removeClass('show');
+  $('#updateme').addClass('show');
+};
+const deleteThis = (e) => {
+  e.preventDefault();
+  $('#content').empty();
+  $('#verifyme').removeClass('show');
+  $('#lookupme').removeClass('show');
+  $('#addme').removeClass('show');
+  $('#containsme').removeClass('show');
+  $('#updateme').removeClass('show');
+  $('#printme').removeClass('show');
+  $('#deleteme').addClass('show');
+};
+const containsThis = (e) => {
+  e.preventDefault();
+  $('#content').empty();
+  $('#verifyme').removeClass('show');
+  $('#lookupme').removeClass('show');
+  $('#addme').removeClass('show');
+  $('#deleteme').removeClass('show');
+  $('#updateme').removeClass('show');
+  $('#printme').removeClass('show');
+  $('#containsme').addClass('show');
+};
+$('#print').on('click', printThis);
+$('#verify').on('click', verifyThis);
+$('#lookup').on('click', lookupThis);
+$('#add').on('click', addThis);
+$('#contains').on('click', containsThis);
+$('#update').on('click', updateThis);
+$('#delete').on('click', deleteThis);
+
+
+$('#print').on('click', function (e) {
+  e.preventDefault();
+  $('#content').empty();
+  employeeList.forEach(e => {
+    render(e.name, e.officeNum, e.phoneNum);
+  });
+
+});
+
+
+const LookupIn = function (e) {
+  e.preventDefault();
+  $('#lookupInput').empty();
+  let LookupName = $('#lookupInput').val();
+  let lookupok = employeeList.filter(e => e.name === LookupName);
+  lookupok.forEach(e => {$('#content').append(`${e.name} ${e.officeNum} ${e.phoneNum}`)})
+};
+$('#submitLookup').on('click', LookupIn);
+
+const containsIn = function (e) {
+  e.preventDefault();
+  $('#containsInput').empty();
+  let containsInfo = $('#containsInput').val();
+    let contained = employeeList.filter(e =>e.name.toLowerCase().includes(containsInfo)); 
+      contained.forEach(e => {$('#content').append(`${e.name} ${e.officeNum} ${e.phoneNum}`)
+    })
+  }
+
+$('#submitContains').on('click', containsIn);
+
+
+  const eName = $('#updateInput').val();
+  const newNumber = $('#updateInput2').val();
+  if (newNumber === 'office') {
+    const newOfficeNumber = $('#updateInput2').val();
+    for (let i = 0; i < employeeList.length; i++) {
+      if (employeeList[i].name === eName) {
+      let updatedNum = employeeList[i].officeNum = newOfficeNumber
+      updatedNum.forEach(e => {`${e.name} ${e.officeNum} ${e.phoneNum}`});
+      }
+    }
+    }
+   else if (newNumber === 'phone') {
+    const eName = $('#updateInput').val();
+    const newPhoneNumber = $('#updateInput3').val();
+    for (let i = 0; i < employeeList.length; i++) {
+      if (employeeList[i].name === eName) {
+        let updated = employeeList.phoneNum = newPhoneNumber
+          updated.forEach(e => {`${e.name} ${e.officeNum} ${e.phoneNum}`});
+      }
+      }
+    }
+  
+
+const addIn = function (e) {
+  e.preventDefault();
+  $('#addInput').empty();
+  let addName = $('#addInput').val();
+  let addOffice = $('#addInput2').val();
+  let addTele = $('#addInput3').val();
+  let addedEmployee = {
+        name: addName,
+        officeNum: addOffice,
+        phoneNum: addTele
+      };
+
+      employeeList.push(addedEmployee);
+        for (let i = 0; i < employeeList.length; i++) {
+          $('#content').append(`${employeeList[i].name} ${employeeList[i].officeNum} ${employeeList[i].phoneNum}`);
+        }
+      }
+
+$('#submitAdd').on('click', addIn);
+
+
+const deleteIn = function (e) {
+  e.preventDefault();
+  $('#deleteInput').empty();
+  deleteInfo = $('#deleteInput').val();
+
+  for (let i = 0; i < employeeList.length; i++) {
+    if (deleteInfo === employeeList[i].name) {
+      employeeList.splice(i, 1);
+      employeeList.forEach(e => {$('#content').append(`${e.name} ${e.officeNum} ${e.phoneNum}`)
+      });
+    };
+  };
+};
+$('#submitDelete').on('click', deleteIn);
+
+const verifyIn = function (e) {
+  e.preventDefault();
+  let msg = 'Employee Not Found';
+  $('#verifyInput').empty();
+  verifyInfo = $('#verifyInput').val();
+  for (let i = 0; i < employeeList.length; i++) {
+    if (verifyInfo === employeeList[i].name) {
+      msg = 'Employee Found';
+    }
+  }
+  $('#content').text(msg);
+};
+
+$('#submitVerify').on('click', verifyIn);
